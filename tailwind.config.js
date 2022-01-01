2290906395ecbe1a504e1992ec9829592c426119
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-01 19:00:35 -0300

    :tada: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Roboto', "sans-serif"],
+      title: ['Inter', "sans-serif"]
+    },
+    colors: {
+      body: "#9CA3AF",
+      heading: "#E5E7EB",
+      highlight: "#0F766E"
+    }
+  },
+}
