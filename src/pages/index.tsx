bd47318d57d8e0b10d06036bcbddf29929709ad8
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-02 02:54:29 -0300

    feat(pages): Create uses's username selection page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,75 @@
+import { useState } from 'react';
 import Image from 'next/image';
+import { FiCheck, FiX } from 'react-icons/fi';
 
 import GoogleIcon from '../assets/icons/google.svg';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const [renderCreateUsernameView, setRenderCreateUsernameView] =
+    useState(true);
+  const [username, setUsername] = useState('');
+  const [isValidUsername, setIsValidUsername] = useState(true);
+
+  const router = useRouter();
+
+  function handleLogin() {
+    setRenderCreateUsernameView(true);
+  }
+
+  if (renderCreateUsernameView)
+    return (
+      <main className="container mx-auto px-4 pt-20">
+        <p className="font-bold text-2xl text-heading tracking-wider md:w-1/4 pb-2">
+          Antes de começarmos, escolha seu nome de usuário.
+        </p>
+        <p className="text-lg text-body pb-8">
+          Ele vai servir para identificar você dentro do Queenmovl
+        </p>
+
+        <label className="flex items-center h-8 w-full md:w-72 rounded-md px-2 mb-2 text-body bg-gray-700">
+          <span className="opacity-50">@</span>
+          <input
+            type="text"
+            className="bg-transparent flex-1 outline-none"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+
+        {!username ? (
+          ''
+        ) : isValidUsername ? (
+          <div className="flex items-center">
+            <FiCheck className="text-2xl text-highlight mr-2" />
+            <span className="text-xs text-body">Nome de usuário válido</span>
+          </div>
+        ) : (
+          <div className="flex items-center">
+            <FiX className="text-2xl text-error mr-2" />
+            <span className="text-xs text-body">
+              Este nome de usuário já está sendo usado
+            </span>
+          </div>
+        )}
+
+        <button
+          className="
+            flex items-center justify-center
+            bg-highlight rounded-md h-14 p-4 shadow-md
+            transition hover:brightness-90 active:filter-none
+            font-mono uppercase tracking-wider text-heading
+            w-full md:w-72 mt-20
+            disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:filter-none
+          "
+          disabled={!username || !isValidUsername}
+          onClick={() => handleLogin()}
+        >
+          Começar
+        </button>
+      </main>
+    );
+
   return (
     <main className="container mx-auto px-4 pt-20">
       <p className="font-title text-heading text-4xl leading-snug tracking-wider pb-4 md:max-w-xl">
@@ -21,6 +88,7 @@ export default function Home() {
           font-mono uppercase tracking-wider text-heading
           w-full md:w-auto
         "
+        onClick={() => handleLogin()}
       >
         <Image src={GoogleIcon} alt="Google icon" />
         Entrar com o Google
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors');
+
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
@@ -8,7 +11,8 @@ module.exports = {
       colors: {
         body: "#9CA3AF",
         heading: "#E5E7EB",
-        highlight: "#0F766E"
+        highlight: "#0F766E",
+        error: colors.red["600"]
       },
       fontFamily: {
         sans: ['Roboto', "sans-serif"],
