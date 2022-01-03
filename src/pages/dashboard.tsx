8d6849dd0a3841eb0a9f316706b2a6a8865b0a43
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-03 16:44:37 -0300

    feat(pages): Create Dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <main className="container mx-auto px-4 pt-20">
+      <p className="text-lg text-body text-center">
+        Selecione ou crie uma lista ao lado para começar =)
+      </p>
+    </main>
+  );
+}
