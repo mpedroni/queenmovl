9bc51afa8bdeef2a145fc945ddc2f6c4a601d04a
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-03 21:28:30 -0300

    feat(hooks): Create SidebarDrawer context

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+interface SidebarDrawerProviderProps {
+  children: ReactNode;
+}
+
+interface SidebarDrawerContextData {
+  isOpen: boolean;
+  close: () => void;
+  open: () => void;
+  handleState: () => void;
+}
+
+const SidebarDrawerContext = createContext<SidebarDrawerContextData>(
+  {} as SidebarDrawerContextData
+);
+
+export function SidebarDrawerProvider({
+  children,
+}: SidebarDrawerProviderProps) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+
+  function handleState() {
+    isOpen ? close() : open();
+  }
+
+  return (
+    <SidebarDrawerContext.Provider value={{ isOpen, open, close, handleState }}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+}
+
+export function useSidebarDrawer() {
+  const context = useContext(SidebarDrawerContext);
+
+  return context;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from 'next/app';
+
 import '../styles/globals.css';
 
 import { AuthProvider } from '../contexts/AuthContext';
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 
 import { Header } from '../components/Header';
+import { SidebarDrawer } from '../components/SidebarDrawer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Header />
+      <SidebarDrawerProvider>
+        <Header />
 
+        <SidebarDrawer />
+      </SidebarDrawerProvider>
       <Component {...pageProps} />
     </AuthProvider>
   );
