fe4267fab3305e4edc873657fd0c3e2514d5d219
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-20 23:17:33 -0300

    fix(mirage): Correct the returned data props

--- src/services/miragejs/index.ts
+++ src/services/miragejs/index.ts
@@ -7,18 +7,12 @@ export function makeServer() {
 
       this.get('/users/:id/lists', () => ({
         lists: [
-          { id: 1, name: 'Filmes' },
-          { id: 2, name: 'Livros' },
-          { id: 3, name: 'Filmes de terror' },
-          { id: 4, name: 'Filmes do oscar' },
-          { id: 5, name: 'Livros técnicos' },
+          { id: 1, title: 'Filmes' },
+          { id: 2, title: 'Livros' },
+          { id: 3, title: 'Filmes de terror' },
+          { id: 4, title: 'Filmes do oscar' },
+          { id: 5, title: 'Livros técnicos' },
         ],
-      }));
-
-      this.get('/me', () => ({
-        name: 'Matheus Pedroni',
-        username: 'mpedroni',
-        avatar_url: 'https://github.com/mpedroni.png',
       }));
     },
   });
